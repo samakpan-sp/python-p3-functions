72aa5ef6ab02f9b72df3824133a8855a7981c040
samakpan-sp <samakpan817@gmail.com>
2023-09-26 23:44:02 +0100

    Completed function in python

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print('Hello, programmer!')
+greet_programmer()
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet('Samuel')
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
 
 def add(num1, num2):
-    pass
+    return num1 + num2
+add(3, 9)
 
 def halve(number):
-    pass
+    return number / 2
+
+print(halve(10))
